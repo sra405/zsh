355400ad575b36aabada0f274735ab6f4ec2a5a2
Scott Adams <scott.adams@raspberrypi.org>
2024-11-21 13:58:10 +0000

    feat(initial-setup): trial using starship

---
+++ aliases.zsh
@@ -0,0 +1,35 @@
+alias ls='ls -G' # colorize `ls` output
+alias zshreload='source ~/.zshrc' # reload ZSH
+alias grep='grep --color=auto' # colorize `grep` output
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias less='less -R'
+alias g='git'
+
+alias rm='rm -i' # confirm removal
+alias cp='cp -i' # confirm copy
+alias mv='mv -i' # confirm move
+
+# locations
+alias repos='cd $HOME/Documents/repos'
+alias le='cd $HOME/Documents/repos/learner-experience'
+alias personal='cd $HOME/Documents/repos/personal'
+alias infra='cd $HOME/Documents/repos/infra'
+alias shared='cd $HOME/Documents/repos/shared'
+
+# git
+alias pull='git pull'
+alias push='git push'
+alias cm='cz commit'
+alias co='git checkout'
+alias main='git checkout main'
+
+# docker
+alias up='docker-compose up -d'
+alias down='docker-compose down'
+alias build='docker-compose build'
+
+# yarn/npm
+alias yi='yarn install'
+alias ni='npm i'
---
+++ plugins.zsh
@@ -0,0 +1,41 @@
+# stealing oh-my-zsh keybindings
+source $HOME/.zsh/key-bindings.zsh
+
+# setup using https://gist.github.com/raftheunis87/607682946d0ef041ce1ad28c37456b7d
+# syntax highlighting plugin
+source $HOME/.zsh/fast-syntax-highlighting/F-Sy-H.plugin.zsh
+
+# completion plugin
+source $HOME/.zsh/completion.zsh
+
+autoload -Uz compinit
+typeset -i updated_at=$(date +'%j' -r ~/.zcompdump 2>/dev/null || stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null)
+if [ $(date +'%j') != $updated_at ]; then
+  compinit -i
+else
+  compinit -C -i
+fi
+zmodload -i zsh/complist
+
+# autosuggestions plugin
+source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# history plugin
+source $HOME/.zsh/history.zsh
+
+# substring search
+source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+#bindkey "\e[1~" beginning-of-line
+#bindkey "\e[4~" end-of-line
+#bindkey "\e[7~" beginning-of-line
+#bindkey "\e[8~" end-of-line
+#bindkey "\eOH" beginning-of-line
+#bindkey "\eOF" end-of-line
+#bindkey "\e[H" beginning-of-line
+#bindkey "\e[F" end-of-line
+
+# aliases
+source $HOME/.zsh/aliases.zsh
