81d957f650d80bcb392952aab0a92e03ce35aad3
Scott Adams <scott.adams@raspberrypi.org>
2025-04-09 14:15:23 +0100

    fix(aliases): additional

--- aliases.zsh
+++ aliases.zsh
@@ -5,9 +5,8 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias less='less -R'
-alias g='git'
 
-alias rm='rm -i' # confirm removal
+alias rm='rm -ri' # confirm removal
 alias cp='cp -i' # confirm copy
 alias mv='mv -i' # confirm move
 
@@ -20,6 +19,7 @@ alias shared='cd $HOME/Documents/repos/shared'
 alias ed='cd $HOME/Documents/repos/educators'
 
 # git
+alias g='git'
 alias pull='git pull'
 alias push='git push'
 alias cm='git add . && cz commit'
@@ -27,12 +27,16 @@ alias co='git checkout'
 alias main='git checkout main'
 
 # docker
-alias up='docker-compose up -d'
-alias down='docker-compose down'
-alias build='docker-compose build'
+alias dc='docker compose'
+alias up='docker compose up -d'
+alias down='docker compose down'
+alias build='docker compose build'
 
 # yarn/npm
 alias yi='yarn install'
 alias ni='npm i'
 alias yr='yarn run'
 alias nr='npm run'
+
+# misc
+alias tf='terraform'
